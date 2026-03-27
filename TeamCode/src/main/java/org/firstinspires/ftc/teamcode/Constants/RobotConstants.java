95672ed9f2a0208fba3513d2b900d350f8f85053
felicity <25021@hnhs.school.nz>
2026-03-27 17:01:00 +1300

    Outtake subsystem created

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/RobotConstants.java
@@ -14,4 +14,21 @@ public class RobotConstants {
     public static final int TICKS_PER_ROTATION = 538;
     public static final int GEAR_RATIO = 144;
 
+    // Outtake constants
+
+    public static final double OUTTAKE_POWER = 0.9;
+
+    public static final double OUTTAKE_Velocity = 1000;
+
+    public static final double BOTTOM_ANGLE = 31.22;
+
+    public static final double ANGLE_DIFFERENCE = 18.73;
+
+    public static final double EXTENDED_SERVO_POSITION = 1;
+
+
+
+
+
+
 }
